fc3d135a8f02f532812159d96b9c8f3285a5896e
Hiroyuki Sano <sh19910711@gmail.com>
2013-04-02 17:21:25 +0900

    grunt watchできるようにした

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,29 @@
 
     module.exports = function( grunt ) {
         grunt.initConfig({
+            
             sass: {
-                dist: {
+                application: {
                     files: {
                         'css/application/main.css': ['scss/application/main.scss']
                     }
                 }
+            },
+
+            watch: {
+                application: {
+                    files: ['scss/application/*.scss'],
+                    tasks: ['sass'],
+                    options: {
+                        nospawn: true
+                    }
+                }
             }
+
         });
 
         grunt.loadNpmTasks('grunt-contrib-sass');
+        grunt.loadNpmTasks('grunt-contrib-watch');
 
         grunt.registerTask('default', ['sass']);
     };
